d633958176bad89c7c4093a9ace0ddc073c33ff9
Brent Graves <graves.brent@gmail.com>
2022-05-17 16:47:04 -0400

    hero carousel is cycling throught images on index.html

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
-// source https://www.htmlelements.com/demos/carousel/ 
+// source https://www.w3schools.com/howto/howto_js_slideshow.asp
+let slideIndex = 0;
+showSlides();
 
-window.onload = function () {
-    var basePath = '/images/',
-        carouselSquare = document.getElementById('carouselSquare');
-     
-    carouselSquare.dataSource = generateDataSource(7);
-     
-    function generateDataSource(items) {
-        let dataSource = Array(items).fill({});
-    
-        dataSource.forEach((element, index) => dataSource[index] = { image: `${basePath}carousel-square-${index + 1}.jpg` });
-        return dataSource;
-    }
-    }
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"; 
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1} 
+  slides[slideIndex-1].style.display = "block"; 
+  setTimeout(showSlides, 5000); // Change image every 5 seconds
+}
